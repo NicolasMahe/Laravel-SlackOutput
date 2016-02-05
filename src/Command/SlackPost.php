08850d88e551a9974ea9759f8f10e170fabca3f1
Nicolas Mahé <nicolas@mahe.me>
2016-02-05 10:30:16 +0700

    Can send multiple attachments

--- src/Command/SlackPost.php
+++ src/Command/SlackPost.php
@@ -45,7 +45,13 @@ class SlackPost extends Command {
 
     //attach data
     if (is_array($attach)) {
-      $client = $client->attach($attach);
+	    if ($this->is_assoc($attach)) {
+		    $attach = [$attach];
+	    }
+
+	    foreach($attach as $attachElement) {
+		    $client = $client->attach($attachElement);
+	    }
     }
     if (!empty($to)) {
       $client = $client->to($to);
@@ -80,4 +86,19 @@ class SlackPost extends Command {
     return $client;
   }
 
+	/**
+	 * Check if array is associative
+	 *
+	 * @param array $array
+	 * @return bool
+	 */
+	public static function is_assoc(array $array)
+	{
+		// Keys of the array
+		$keys = array_keys($array);
+
+		// If the array keys of the keys match the keys, then the array must
+		// not be associative (e.g. the keys array looked like {0:0, 1:1...}).
+		return array_keys($keys) !== $keys;
+	}
 }
